06dc8f7c516a9d5c9499995a016e07e2fbce5b9f
OneTallProgrammer <jmcauthen@live.com>
2019-09-13 15:14:10 -0700

    Added placeholder files for upload.html preprocess.html data_analysis.html feature_extract.html classify.html

--- workbench/urls.py
+++ workbench/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('upload', views.upload, name='upload'),
+    path('preprocess', views.preprocess, name='preprocess'),
+    path('data_analysis', views.data_analysis, name='data_analysis'),
+    path('feature_extract', views.feature_extract, name='feature_extract'),
+    path('classify', views.classify, name='classify'),
 ]
--- workbench/views.py
+++ workbench/views.py
@@ -2,4 +2,24 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'workbench/base.html', {'title': 'EEG Workbench'})
+    return render(request, 'workbench/base.html')
+
+
+def upload(request):
+    return render(request, 'workbench/upload.html')
+
+
+def preprocess(request):
+    return render(request, 'workbench/preprocess.html')
+
+
+def data_analysis(request):
+    return render(request, 'workbench/data_analysis.html')
+
+
+def feature_extract(request):
+    return render(request, 'workbench/feature_extract.html')
+
+
+def classify(request):
+    return render(request, 'workbench/classify.html')
