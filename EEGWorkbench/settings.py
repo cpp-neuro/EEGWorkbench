e8900a43d43e8bb8d0ef89914a9c73da978a5103
OneTallProgrammer <jmcauthen@live.com>
2019-09-13 14:18:42 -0700

    Configured base.html Added base.css Added static file tree

--- EEGWorkbench/settings.py
+++ EEGWorkbench/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'workbench'
 ]
 
 MIDDLEWARE = [
@@ -82,8 +83,7 @@ DATABASES = {
 }
 
 
-# Password validation
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+# Password validation # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
