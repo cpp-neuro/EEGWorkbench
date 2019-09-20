f2104e6b85950cd2eeb22da38f3d8c07e5ac1ad0
OneTallProgrammer <jmcauthen@live.com>
2019-09-20 12:35:42 -0700

    Added upload functionality to upload.html

--- workbench/urls.py
+++ workbench/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('upload', views.upload, name='upload'),
-    path('preprocess', views.preprocess, name='preprocess'),
-    path('data_analysis', views.data_analysis, name='data_analysis'),
-    path('feature_extract', views.feature_extract, name='feature_extract'),
-    path('classify', views.classify, name='classify'),
+    path('upload', views.upload_page, name='upload'),
+    path('preprocess', views.preprocess_page, name='preprocess'),
+    path('data_analysis', views.data_analysis_page, name='data_analysis'),
+    path('feature_extract', views.feature_extract_page, name='feature_extract'),
+    path('classify', views.classify_page, name='classify'),
+    path('upload_file', views.upload_file, name='upload_file'),
 ]
--- workbench/views.py
+++ workbench/views.py
@@ -1,25 +1,44 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.core.files.storage import FileSystemStorage
+from .forms import UploadedFileForm
+
+import os
+from EEGWorkbench.settings import BASE_DIR
+
+UPLOAD_DIR = os.path.join(BASE_DIR, 'workbench/uploaded_files')
 
 
 def index(request):
-    return render(request, 'workbench/base.html')
+    return redirect('upload')
 
 
-def upload(request):
-    return render(request, 'workbench/upload.html')
+def upload_page(request):
+    return render(request, 'workbench/upload.html', {'uploaded_file_url': ''})
 
 
-def preprocess(request):
+def preprocess_page(request):
     return render(request, 'workbench/preprocess.html')
 
 
-def data_analysis(request):
+def data_analysis_page(request):
     return render(request, 'workbench/data_analysis.html')
 
 
-def feature_extract(request):
+def feature_extract_page(request):
     return render(request, 'workbench/feature_extract.html')
 
 
-def classify(request):
+def classify_page(request):
     return render(request, 'workbench/classify.html')
+
+
+def upload_file(request):
+    if request.method == 'POST' and request.FILES['file']:
+        file = request.FILES['file']
+        fs = FileSystemStorage(location=UPLOAD_DIR)
+        filename = fs.save(file.name, file)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'workbench/upload.html', {'uploaded_file_url': uploaded_file_url})
+    print("Not saved")
+    return redirect('upload')
